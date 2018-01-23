5b335516c568edad592ac16dfc2be0a23a2b84eb
Jonathan R. Jones <jonathan.r.jones.3@gmail.com>
2018-01-23 12:43:33 -0500

    Jan-23-2018_12:43_PM

--- Hello_World_2.bat
+++ Hello_World_2.bat
@@ -83,11 +83,7 @@ echo Usage: hello_world.bat [single parameter]
 echo.
 echo     Parameter  Description
 echo -------------  -----------------------------------------------------
-echo        step_1  Function that does [blank].
 echo        step_2  Function that does [blank].
-echo        step_3  Function that does [blank].
-echo     all_steps  Function that runs all functions.
-echo    some_steps  Function that runs some functions.
 
 goto exitb
 
@@ -95,9 +91,9 @@ goto exitb
 
 :_
 
-:step_1
+:step_2
 
-set fp=* Step 1. A call to this batch file.
+set fp=* Step 2. A call to another batch file.
 
 rem lu: Jan-23-2018
 
@@ -105,81 +101,6 @@ echo %fp%
 echo.
 
 echo Put some code here.
-
-goto exitb
-
-
-
-:_
-
-:step_3
-
-set fp=* Step 3.
-
-rem lu: Jan-23-2018
-
-echo %fp%
-echo.
-
-echo Put some code here.
-
-goto exitb
-
-
-
-:_
-
-:all_steps
-
-set fp=* All steps.
-
-rem lu: Jan-23-2018
-
-echo %fp%
-
-call %0 step_1
-
-call hello_world_2 step_2
-
-call %0 step_3
-
-goto exitb
-
-
-
-:_
-
-:some_steps
-
-set fp=* Some steps.
-
-rem lu: Jan-23-2018
-
-echo %fp%
-
-call %0 step_1
-
-call hello_world_2 step_2
-
-goto exitb
-
-
-
-:_
-
-:all_steps_style_2
-
-set fp=* All steps calling style 2.
-
-rem lu: Jan-23-2018
-
-echo %fp%
-
-call :step_1
-
-call hello_world_2 step_2
-
-call :step_3
 
 goto exitb
 
