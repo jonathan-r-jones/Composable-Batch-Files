28e2d402d6ad25092a4ddcbe51d5fe5b299ef0fc
Jonathan R. Jones <jonathan.r.jones.3@gmail.com>
2018-02-01 18:23:02 -0500

    Feb-1-2018_6:23_PM

---
+++ ed.bat
@@ -0,0 +1,132 @@
+:_
+
+@echo off
+
+
+
+:_
+
+set fp=* Execute application.
+
+echo.
+echo %fp%
+
+
+
+:_
+
+set fp=* Route callers.
+
+if "%1" == "" goto help
+
+if "%1" == "/?" goto help
+
+if "%1" == "help" goto help
+
+
+
+:_
+
+set fp=* Detect a period in the first parameter and run.
+
+rem lu: Feb-1-2018
+
+echo %1 | find /i ".">nul
+
+if %errorlevel% == 0 set cbf_filename=%1
+if %errorlevel% == 1 call fn %1
+rem qq-1
+
+set cbf_parameter=%cbf_filename%
+
+if "%2" == "" call an me
+if not "%2" == "" call an %2
+
+call r
+
+
+goto exitb
+
+
+
+:_
+
+:help
+
+set fp=* Edit file.
+
+echo File Purpose (fp): %fp%
+
+echo.
+echo Filename stands for: EDit file.
+
+echo.
+echo Last Updated (lu): Feb-1-2018
+
+echo.
+echo Usage: %0 [Parameter 1]
+
+echo.
+echo Parameter 1: The filename nickname of the file to execute or filename of a file in the current folder.
+rem qq-1
+
+goto exitb
+
+
+
+:_+ Exit Functions
+
+
+
+::_
+
+:exit
+
+set fp= * Exit.
+
+rem echo %fp%
+
+exit
+
+
+
+::_
+
+:exitb
+
+set fp= * Exit batch file but not command window.
+
+exit /b
+
+
+
+::_
+
+:exitp
+
+set fp= * Exit with pause.
+
+echo.
+rem echo %fp%
+
+pause
+
+exit
+
+
+
+::_
+
+rem ******* End Exit Functions.
+
+
+
+:_
+
+:code_execution_area
+
+set fp=* Code below here runs.
+
+
+
+:_ (!rfsp) (mov-9)
