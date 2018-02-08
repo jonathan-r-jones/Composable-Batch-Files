97452a59ad3c2d492f2bd4edd83e622699332e35
Jonathan R. Jones <jonathan.r.jones.3@gmail.com>
2018-02-08 15:15:39 -0500

    Feb-8-2018_3:15_PM

--- g.bat
+++ g.bat
@@ -1647,62 +1647,6 @@ goto exitb
 
 
 
-:_+ Author
-
-
-
-::_
-
-:author_git
-
-set fp=* Configure GitHub author and email. Note: Please don't confuse this with the Bitbucket author signature.
-
-echo %fp%
-echo.
-
-@echo on
-git config --global user.name "Jonathan R. Jones"
-git config --global user.email "jonathan.r.jones.3@gmail.com"
-@echo off
-
-goto exitb
-
-
-
-::_
-
-:author_bit
-
-set fp=* Configure BitBucket author and email. Note: Please don't confuse this with the GitHub author signature.
-
-echo %fp%
-
-@echo on
-git config --global user.name "Jonathan17"
-git config --global user.email "jonathan.r.jones@nesassociates.com"
-@echo off
-
-goto exitb
-
-
-
-::_
-
-:author_devops
-
-set fp=* Configure BitBucket author and email.
-
-echo %fp%
-
-@echo on
-git config --global user.name "mercury_devops"
-git config --global user.email "nes.mercury@nesassociates.com"
-@echo off
-
-goto exitb
-
-
-
 :_+ Stash and Pop
 
 
@@ -2053,6 +1997,61 @@ rem Creation Date: May-5-2017
 echo %fp%
  
 git checkout *.*
+
+goto exitb
+
+
+
+:_+ Author
+
+
+
+::_
+
+:au
+
+:author_git
+
+set fp=* Configure GitHub author and email. Note: Please don't confuse this with the Bitbucket author signature.
+
+echo %fp%
+
+git config --global user.name "Jonathan R. Jones"
+git config --global user.email "jonathan.r.jones.3@gmail.com"
+
+goto exitb
+
+
+
+::_
+
+:author_bit
+
+set fp=* Configure BitBucket author and email. Note: Please don't confuse this with the GitHub author signature.
+
+echo %fp%
+
+@echo on
+git config --global user.name "Jonathan17"
+git config --global user.email "jonathan.r.jones@nesassociates.com"
+@echo off
+
+goto exitb
+
+
+
+::_
+
+:author_devops
+
+set fp=* Configure BitBucket author and email.
+
+echo %fp%
+
+@echo on
+git config --global user.name "mercury_devops"
+git config --global user.email "nes.mercury@nesassociates.com"
+@echo off
 
 goto exitb
 
