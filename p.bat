023984094a95d737517de2632d1477c479e423c6
Jonathan R. Jones <jonathan.r.jones.3@gmail.com>
2018-12-20 15:45:40 -0500

    Dec-20-2018_3:45_PM

--- p.bat
+++ p.bat
@@ -51,7 +51,8 @@ echo.
 echo Usage: %0 [Parameter 1]
 
 echo.
-echo Parameter 1 (Optional): The folder you wish to switch to. If left blank, the current folder is used.
+echo Parameter 1 (Optional): The folder you wish to switch to. If left blank, the current 
+echo folder is used.
 
 exit/b
 
